bf978b5218b783a31df5bace77056eb9855b3ce3
Aarya Rastogi <97822457+aaryarastogi@users.noreply.github.com>
2026-07-09 14:13:58 +0530

    Add: find-duplicate-integer - submission-2

---
+++ Data Structures & Algorithms/find-duplicate-integer/submission-2.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int findDuplicate(int[] nums) {
+        for(int i=0;i<nums.length;i++){
+            int index = (nums[i]<0 ? nums[i]*-1 : nums[i]) -1;
+
+            if(nums[index] < 0){
+                return (nums[i]<0 ? nums[i]*-1 : nums[i]);
+            }
+            nums[index] *= -1;
+        }
+        return -1;
+    }
+}
